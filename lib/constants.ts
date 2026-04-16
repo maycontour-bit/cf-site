f1d25aca3bd1e54b880f8586e618340ac376446e
maycontour-bit <maycon.tour@gmail.com>
2026-04-16 20:43:30 -0300

    Add lib/constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,22 @@
+// Cores oficiais
+export const COLORS = {
+  navy: '#002147',
+  navyDark: '#001229',
+  gold: '#C4A35A',
+  goldLight: '#D4B97A',
+  grayLead: '#4A4A4A',
+  grayMuted: '#7A8A9A',
+  white: '#FDFDFD',
+  redDanger: '#C0392B',
+  greenSuccess: '#27AE60',
+  footerBg: '#000D1A',
+} as const
+
+export const WHATSAPP_URL = 'https://wa.me/5541984301488?text=Ol%C3%A1!%20Gostaria%20de%20agendar%20meu%20Diagn%C3%B3stico%20VIPE%20gratuito.'
+
+export const CALCULATOR_URLS = {
+  aposentadoria: 'https://site.ocasteloforte.com/calculadora/simulador-de-aposentadoria/',
+  alquiler: 'https://site.ocasteloforte.com/calculadora/alugar-ou-financiar-um-imovel/',
+  emergencia: 'https://site.ocasteloforte.com/calculadora/calculadora-reserva-de-emergencia/',
+  juros: 'https://site.ocasteloforte.com/calculadora/simulador-de-juros-compostos/',
+} as const
